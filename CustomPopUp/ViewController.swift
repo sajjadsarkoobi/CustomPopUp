efd896ecd5bdd5b7fa4b18f1adf946ae46bfbccc
Sajjad Sarkoobi <sajjadsarkoobi@gmail.com>
2022-07-08 19:49:19 +0300

    Create base project with button

--- CustomPopUp/ViewController.swift
+++ CustomPopUp/ViewController.swift
@@ -9,6 +9,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var button: UIButton!
+    @IBAction func buttonAction(_ sender: UIButton) {
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
